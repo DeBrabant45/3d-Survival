eb3f479b2ea13f8299edc1a1110cc25e1b833194
Aaron <DeBrabant45@users.noreply.github.com>
2020-12-12 15:20:38 -0500

    Changing file structures and adding ScriptableObjects

---
+++ Assets/Scripts/ScriptableObjects/ItemsSO/ItemSO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemSO : ScriptableObject, ISerializationCallbackReceiver
+{
+    [SerializeField] private string _iD;
+    [SerializeField] private string _itemName;
+    [SerializeField] private Sprite _imageSprite;
+    [SerializeField] private GameObject _model;
+    [SerializeField] private bool _isStackable;
+    [SerializeField] [Range(1, 100)] private int _stackLimit = 100;
+    [SerializeField] private ItemType _itemType;
+
+    public void OnAfterDeserialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnBeforeSerialize()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public enum ItemType
+{
+    None,
+    Food,
+    Weapon
+}
