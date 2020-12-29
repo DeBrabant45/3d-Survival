f38cb51feee075fa4438ead12140334768d5ee15
Aaron <DeBrabant45@users.noreply.github.com>
2020-12-29 18:50:30 -0500

    Adding item spawner

--- Assets/Scripts/SpawnScripts/ItemSpawnManager.cs
+++ Assets/Scripts/SpawnScripts/ItemSpawnManager.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ItemSpawnManager : MonoBehaviour
 {
     [SerializeField] private static ItemSpawnManager _instance;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private string _pickableLayerMask;
+    [SerializeField] private Transform _itemSpawnerParent;
 
     public static ItemSpawnManager Instance { get => _instance; }
 
@@ -18,6 +20,12 @@ public class ItemSpawnManager : MonoBehaviour
             _instance = this;
         else if (_instance != this)
             Destroy(gameObject);
+        SpawnAllItems();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAllItems());
     }
 
     public void CreateItemAtPlayersFeet(string itemID, int currentItemCount)
@@ -36,5 +44,46 @@ public class ItemSpawnManager : MonoBehaviour
         pickableItem.SetCount(currentItemCount);
         pickableItem.SetDataSource(ItemDataManager.Instance.GetItemData(itemID));
         itemGameObject.layer = LayerMask.NameToLayer(_pickableLayerMask);
+    }
+
+    private IEnumerator SpawnAllItems()
+    {
+        foreach (Transform itemSpawner in _itemSpawnerParent)
+        {
+            var spawner = itemSpawner.GetComponent<ItemSpawner>();
+            if (spawner != null)
+            {
+                Vector3 randomPosition = GenerateRandomPosition(spawner.Radius);
+                var spawnPosition = itemSpawner.position + randomPosition;
+                if (spawner.SingleObject && spawner.ItemToSpawn.IsStackable)
+                {
+                    CreateItemInPlace(spawnPosition, spawner.ItemToSpawn, spawner.Count);
+                }
+                else
+                {
+                    for (int i = 0; i < spawner.Count; i++)
+                    {
+                        spawnPosition = itemSpawner.position + randomPosition;
+                        CreateItemInPlace(spawnPosition, spawner.ItemToSpawn, spawner.Count);
+                        randomPosition = GenerateRandomPosition(spawner.Radius);
+                    }
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void CreateItemInPlace(Vector3 spawnPosition, ItemSO itemToSpawn, int count)
+    {
+        var itemGameObject = Instantiate(itemToSpawn.Model, spawnPosition, Quaternion.identity, _itemSpawnerParent);
+        PrepareItemGameObject(itemToSpawn.ID, count, itemGameObject);
+    }
+
+    private Vector3 GenerateRandomPosition(float radius)
+    {
+        var xRadius = Random.Range(-radius, radius);
+        var yRadius = Random.Range(0, radius);
+        var zRadius = Random.Range(-radius, radius);
+        return new Vector3(xRadius, yRadius, zRadius);
     }
 }
--- Assets/Scripts/SpawnScripts/ItemSpawner.cs
+++ Assets/Scripts/SpawnScripts/ItemSpawner.cs
@@ -6,5 +6,22 @@ public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] private ItemSO _itemToSpawn;
     [SerializeField] [Range(1, 100)] private int _count = 1;
+    [SerializeField] [Range(0.1f, 50f)] private float _radius = 1;
     [SerializeField] private bool _singleObject = false;
+    [SerializeField] private bool _showGizmo = true;
+    [SerializeField] private Color _gizmoColor = Color.green;
+
+    public bool SingleObject { get => _singleObject; }
+    public ItemSO ItemToSpawn { get => _itemToSpawn; }
+    public int Count { get => _count; }
+    public float Radius { get => _radius; }
+
+    public void OnDrawGizmos()
+    {
+        if(_showGizmo && _radius > 0)
+        {
+            Gizmos.color = _gizmoColor;
+            Gizmos.DrawWireSphere(transform.position, _radius);
+        }
+    }
 }
