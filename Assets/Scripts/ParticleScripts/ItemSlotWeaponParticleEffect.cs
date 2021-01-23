74b79a627916a5a4838f3b3a214f26f59b46d032
Aaron <DeBrabant45@users.noreply.github.com>
2021-01-22 23:09:14 -0500

    Updating weapon sound and particle system

---
+++ Assets/Scripts/ParticleScripts/ItemSlotWeaponParticleEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlotWeaponParticleEffect : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _itemParticleSystem;
+    [SerializeField] private GameObject _itemSlot;
+
+    private void Update()
+    {
+        AddParticleEffects();
+    }
+
+    private void AddParticleEffects()
+    {
+        if (_itemParticleSystem == null)
+        {
+            var particleEffect = _itemSlot.GetComponentInChildren<ParticleSystem>();
+            if (particleEffect != null)
+            {
+                _itemParticleSystem = particleEffect;
+            }
+        }
+    }
+
+    public void PlayParticleEffect()
+    {
+        _itemParticleSystem.Play();
+    }
+}
---
+++ Assets/Scripts/SoundScripts/ItemSlotWeaponSound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlotWeaponSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource _weaponSound;
+    [SerializeField] private GameObject _itemSlot;
+
+
+    private void Update()
+    {
+        AddWeaponSound();    
+    }
+
+    private void AddWeaponSound()
+    {
+        if(_weaponSound == null)
+        {
+            var weaponSound = _itemSlot.GetComponentInChildren<AudioSource>();
+            if (weaponSound != null)
+            {
+                _weaponSound = weaponSound;
+            }
+        }
+    }
+
+    public void PlayWeaponSound()
+    {
+        _weaponSound.PlayOneShot(_weaponSound.clip);
+    }
+}
