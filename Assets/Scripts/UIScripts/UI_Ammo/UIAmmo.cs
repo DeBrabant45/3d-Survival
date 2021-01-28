5681627cacb46bd9ec42cabdf8269de447f5bf68
Aaron <DeBrabant45@users.noreply.github.com>
2021-01-27 21:17:37 -0500

    Adding UI for ammo

---
+++ Assets/Scripts/UIScripts/UI_Ammo/UIAmmo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAmmo : MonoBehaviour
+{
+    [SerializeField] private GameObject _ammoPanel;
+    [SerializeField] private Text _ammoInGunTxt;
+    [SerializeField] private Text _ammoInStorageTxt;
+    [SerializeField] private Image _equippedWeaponIcon;
+
+    private void Start()
+    {
+        //_ammoPanel.SetActive(false);
+    }
+
+    public void ToggleAmmoPanel()
+    {
+        if (_ammoPanel.activeSelf == false)
+        {
+            _ammoPanel.SetActive(true);
+        }
+        else
+        {
+            _ammoPanel.SetActive(false);
+        }
+    }
+
+    public void SetAmmoInGun(int ammoCount)
+    {
+        _ammoInGunTxt.text = ammoCount + "";
+    }
+
+    public void SetStorageAmmoCount(int ammoCount)
+    {
+        _ammoInStorageTxt.text = ammoCount + "";
+    }
+
+    public void SetEquippedWeaponIcon(Image weaponIcon)
+    {
+        _equippedWeaponIcon = weaponIcon;
+    }
+}
