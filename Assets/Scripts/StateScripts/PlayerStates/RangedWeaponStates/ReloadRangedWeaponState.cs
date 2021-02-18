c3155eeb840f0f11e7cf5eb19b48594a92d9b6b8
Aaron <DeBrabant45@users.noreply.github.com>
2021-02-17 23:59:16 -0500

    Line didn't make it in the last change

--- Assets/Scripts/StateScripts/PlayerStates/RangedWeaponStates/ReloadRangedWeaponState.cs
+++ Assets/Scripts/StateScripts/PlayerStates/RangedWeaponStates/ReloadRangedWeaponState.cs
@@ -25,6 +25,7 @@ public class ReloadRangedWeaponState : BaseState
             }
             else
             {
+                controllerReference.TransitionToState(controllerReference.PreviousState);
             }
         }
         else
